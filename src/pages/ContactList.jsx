39373fd4e36960cefe70fe646c42f133c8fab8a0
Nirmal Kumar <nirmal1064@gmail.com>
2023-02-24 22:11:54 +0530

    Read functionality UI completed

--- src/pages/ContactList.jsx
+++ src/pages/ContactList.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Table } from "react-bootstrap";
 import ContactModal from "../components/ContactModal";
+import { useContacts } from "../context/ContactProvider";
 
 const ContactList = () => {
   const { msg, setMsg, errorMsg, setErrorMsg } = useContacts();
   const [showContactModal, setShowContactModal] = useState(false);
   const [type, setType] = useState("");
   const [activeContact, setActiveContact] = useState({});
+  const { contacts } = useContacts();
 
   const closeContactModal = () => {
     setActiveContact({});
@@ -50,6 +52,31 @@ const ContactList = () => {
         <h2 className="text-center">Contact List</h2>
         <Button onClick={handleAdd}>Add Contact</Button>
       </div>
+      <Table striped bordered responsive variant="dark">
+        <thead>
+          <tr>
+            <th style={{ width: "5%" }}>S.No</th>
+            <th style={{ width: "30%" }}>Name</th>
+            <th style={{ width: "30%" }}>Phone</th>
+            <th style={{ width: "30%" }}>Address</th>
+            <th style={{ width: "5%" }}>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact, idx) => (
+            <tr key={idx}>
+              <td>{idx + 1}</td>
+              <td>{contact.name}</td>
+              <td>{contact.phone}</td>
+              <td>{contact.address}</td>
+              <td className="d-flex flex-row justify-content-around">
+                <i className="bi bi-pencil-square"></i>
+                <i className="bi bi-trash3"></i>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </div>
   );
 };
