77eaedb4d38918eb5688eb59930da43c98345950
Nirmal Kumar <nirmal1064@gmail.com>
2023-02-24 22:09:16 +0530

    Added Fetch/Read functionality

--- src/context/ContactProvider.jsx
+++ src/context/ContactProvider.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { supabase } from "../supabase/client";
 
 const ContactContext = createContext({});
@@ -11,11 +11,28 @@ const ContactProvider = ({ children }) => {
   const [msg, setMsg] = useState("");
 
   const addContact = async (contact) => {
-    const { error } = await supabase.from("contacts").insert(contact);
+    const { data, error } = await supabase
+      .from("contacts")
+      .insert(contact)
+      .select();
+    if (data) {
+      setContacts((prevContacts) => [...prevContacts, data[0]]);
+      setMsg("Contact Added Successfully");
+    }
     if (error) {
       setErrorMsg(error.message);
     }
   };
+
+  const fetchAll = async () => {
+    const { data, error } = await supabase.from("contacts").select();
+    if (data) setContacts(data);
+    if (error) setErrorMsg("Error in Fetching Contacts");
+  };
+
+  useEffect(() => {
+    fetchAll();
+  }, []);
 
   return (
     <ContactContext.Provider
@@ -25,7 +42,8 @@ const ContactProvider = ({ children }) => {
         setMsg,
         errorMsg,
         setErrorMsg,
-        addContact
+        addContact,
+        fetchAll
       }}>
       {children}
     </ContactContext.Provider>
